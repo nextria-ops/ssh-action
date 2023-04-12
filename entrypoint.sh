7b5d55500a8dd437a8a30aac598c05b8d8c70600
Chris Beck <chris.beck@nextria.ca>
2023-04-11 23:13:12 -0400

    Init.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 SSH_USER="$1"
 SSH_HOST="$2"
@@ -7,5 +7,5 @@ SSH_KEY="$4"
 SSH_COMMAND="$5"
 
 echo "$SSH_KEY" > /tmp/ssh_key && chmod 600 /tmp/ssh_key
-ssh -o StrictHostKeyChecking=no -i /tmp/ssh_key -p "$SSH_PORT" "$SSH_USER"@"$SSH_HOST" -t "$SSH_COMMAND"
+ssh -o StrictHostKeyChecking=no -i /tmp/ssh_key -p "$SSH_PORT" "$SSH_USER"@"$SSH_HOST" -tt "$SSH_COMMAND ; exit"
 rm -rf /tmp/ssh_key
