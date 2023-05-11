5a57f23f1cfdcf61302142307537225c082974c7
Nextria <postmaster@nextria.ca>
2023-05-11 18:51:27 +0000

    Updated output to stream to action logs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ SSH_PORT="$3"
 SSH_KEY="$4"
 SSH_COMMAND="$5"
 
-echo "$SSH_KEY" > /tmp/ssh_key && chmod 600 /tmp/ssh_key
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/ssh_key "$SSH_USER"@"$SSH_HOST" -p "$SSH_PORT" $SSH_COMMAND
+echo "$SSH_KEY" > /tmp/ssh_key && \
+chmod 600 /tmp/ssh_key && \
+ssh -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -i /tmp/ssh_key \
+    -t "${SSH_USER}@${SSH_HOST}" \
+    -p "$SSH_PORT" "$SSH_COMMAND; exit $?" 2>&1;
 rm -rf /tmp/ssh_key
